e720fcb4b329a9b243b88628476eca473756aa61
Nicolas <nicolasdealmeidasoares71@gmail.com>
2024-09-16 07:47:33 -0300

    Commit day 16/09/2024

---
+++ Milhao/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Milhao;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Milhao/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Milhao;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
