a82e99442daf361d12a3fa12579049a0e57a4a94
Nicolas <slateste295@gmail.com>
2024-09-19 11:50:46 -0300

    Third  commit

--- Milhao/Modelos/Manager.cs
+++ Milhao/Modelos/Manager.cs
@@ -8,21 +8,27 @@ public class Manager
 
     Questao CurrentQuestao;
 
-    void CreateQuestions(Labelpergunta, Button1, Button2, Button3, Button4, Button5)
-    {
+     private void CriarPerguntas(Label labelPergunta, Button btnanswer01, Button btnanswer02, Button btnanswer03, Button btnanswer04, Button btnanswer05)
+  {
         var Q1 = new Questao();
-        Q1.Question = "Qual o tamanho do seu pinto?"
-        Q1.Answer01 = "0.5m";
+        Q1.Question = "Sim?";
+        Q1.Answer01 = "";
         Q1.Answer02 = "";
         Q1.Answer03 = "";
-        Q1.A
+        Q1.Answer04 = "";
+        Q1.Answer05 = "";
     }
-    public void NextQuestion()
-    {
-        var RandomNumber = RandomNumber.Shared.Next(0, Listaquestoes.Count -1),
-        while (Listaquestoes)
-        
-    }
+    public void ProximaQuestao()
+  {
+    var numRandomico = Random.Shared.Next(0, Listaquestoes.Count);
+    while (ResponsedQuestionsList.Contains(numRandomico))
+      numRandomico = Random.Shared.Next(0, Listaquestoes.Count);
+
+    ResponsedQuestionsList.Add(numRandomico);
+    CurrentQuestao = Listaquestoes [numRandomico];
+    
+    CurrentQuestao.Desenha();
+  }
 
     public Manager(Label Labelpergunta, Button Button1, Button Button2, Button Button3, Button Button4, Button Button5)
     {
--- Milhao/Modelos/Questao.cs
+++ Milhao/Modelos/Questao.cs
@@ -12,20 +12,42 @@ public class Questao
     public string? Answer04;
     public string? Answer05;
     public int CorrectAnswer;
-    private Label Labelpergunta;
-    private Button Button1;
-    private Button Button2;
-    private Button Button3;
-    private Button Button4;
-    private Button Button5;
+
+    Label? labelQuestion;
+    Button? btnAnswer01;
+    Button? btnAnswer02;
+    Button? btnAnswer03;
+    Button? btnAnswer04;
+    Button? btnAnswer05;
+
     public void Desenhar()
     {
-        Labelpergunta.Text = labelpergunta;
-        ButtonResposta1.Text = button1;
-        ButtonResposta2.Text = button2;
-        ButtonResposta3.Text = button3;
-        ButtonResposta4.Text = button4;
-        ButtonResposta5.Text = button5;
+        this.labelQuestion!.Text = this.Question;
+        this.btnAnswer01!.Text = this.Answer01;
+        this.btnAnswer02!.Text = this.Answer02;
+        this.btnAnswer03!.Text = this.Answer03;
+        this.btnAnswer04!.Text = this.Answer04;
+        this.btnAnswer05!.Text = this.Answer05;
+    }
+    public Questao(string Question, string Answer01, string Answer02, string Answer03, string Answer04, string Answer05, int CorrectAnswer)
+    {
+        this.Question = Question;
+        this.Answer01 = Answer01;
+        this.Answer02 = Answer02;
+        this.Answer03 = Answer03;
+        this.Answer04 = Answer04;
+        this.Answer05 = Answer05;
+        this.CorrectAnswer = CorrectAnswer;
+    }
+    public void Desenha()
+    {
+        this.labelQuestion!.Text = this.Question;
+        this.btnAnswer01!.Text = this.Answer01;
+        this.btnAnswer02!.Text = this.Answer02;
+        this.btnAnswer03!.Text = this.Answer03;
+        this.btnAnswer04!.Text = this.Answer04;
+        this.btnAnswer05!.Text = this.Answer05;
+
     }
 
 
